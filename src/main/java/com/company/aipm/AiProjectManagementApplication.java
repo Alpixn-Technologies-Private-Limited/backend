74d38a2faee97c17d7fac9be2607b8cd75d840ae
pooja0804-Alpixn <pooja_ptm@alpixn.com>
2025-07-09 19:16:23 +0530

    initial folder structure

---
+++ src/main/java/com/company/aipm/AiProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.company.aipm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/aipm/AiProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.aipm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
